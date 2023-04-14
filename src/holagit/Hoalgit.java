35d69d2b136636a418c1f7bb32289f640943c79c
jdelgadofernandez <yoquesejoselola@gmail.com>
2023-04-14 09:59:43 +0200

    COMMINT RAPIDO

--- src/holagit/Hoalgit.java
+++ src/holagit/Hoalgit.java
@@ -3,7 +3,7 @@ package holagit;
 public class Hoalgit {
 
 	public static void main(String[] args) {
-		System.out.println("Hola de  nuevo Git dsde elipsazo !");
+		System.out.println("Hola de  nuevo Git dsde elipsazo xcascvSDV!");
 	}
 
 }
